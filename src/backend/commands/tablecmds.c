699cb5105ed902a81b5b8f1d7282bf2447eebe7e
Yugo Nagata <nagata@sraoss.co.jp>
2023-05-31 19:08:51 +0900

    Add DISTINCT support for IVM
    When IMMV is created with DISTINCT, multiplicity of tuples is
    counted and stored in  "__ivm_count__" column, which is a hidden
    column of IMMV.  The value in __ivm_count__ is updated when IMMV
    is maintained incrementally. A tuple in IMMV can be removed if
    and only if the count becomes zero.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -60,6 +60,7 @@
 #include "catalog/toasting.h"
 #include "commands/comment.h"
 #include "commands/defrem.h"
+#include "commands/matview.h"
 #include "commands/event_trigger.h"
 #include "commands/extension.h"
 #include "commands/matview.h"
@@ -3929,6 +3930,14 @@ renameatt_internal(Oid myrelid,
 	 */
 	targetrelation = relation_open(myrelid, AccessExclusiveLock);
 	renameatt_check(myrelid, RelationGetForm(targetrelation), recursing);
+
+	/*
+	 * Don't rename IVM columns.
+	 */
+	if (RelationIsIVM(targetrelation) && isIvmName(oldattname))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("IVM column can not be renamed")));
 
 	/*
 	 * if the 'recurse' flag is set then we are supposed to rename this

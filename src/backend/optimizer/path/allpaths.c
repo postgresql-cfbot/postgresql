696f50c1845eee74c49f93b12383687223325c79
Richard Guo <guofenglinux@gmail.com>
2023-12-15 15:07:08 +0800

    Adjust tuples estimate for appendrel

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -958,6 +958,7 @@ set_append_rel_size(PlannerInfo *root, RelOptInfo *rel,
 {
 	int			parentRTindex = rti;
 	bool		has_live_children;
+	double		parent_tuples;
 	double		parent_rows;
 	double		parent_size;
 	double	   *parent_attrsizes;
@@ -995,6 +996,7 @@ set_append_rel_size(PlannerInfo *root, RelOptInfo *rel,
 	 * have zero rows and/or width, if they were excluded by constraints.
 	 */
 	has_live_children = false;
+	parent_tuples = 0;
 	parent_rows = 0;
 	parent_size = 0;
 	nattrs = rel->max_attr - rel->min_attr + 1;
@@ -1161,6 +1163,7 @@ set_append_rel_size(PlannerInfo *root, RelOptInfo *rel,
 		 */
 		Assert(childrel->rows > 0);
 
+		parent_tuples += childrel->tuples;
 		parent_rows += childrel->rows;
 		parent_size += childrel->reltarget->width * childrel->rows;
 
@@ -1213,10 +1216,10 @@ set_append_rel_size(PlannerInfo *root, RelOptInfo *rel,
 			rel->attr_widths[i] = rint(parent_attrsizes[i] / parent_rows);
 
 		/*
-		 * Set "raw tuples" count equal to "rows" for the appendrel; needed
-		 * because some places assume rel->tuples is valid for any baserel.
+		 * Set "raw tuples" count for the appendrel; needed because some places
+		 * assume rel->tuples is valid for any baserel.
 		 */
-		rel->tuples = parent_rows;
+		rel->tuples = parent_tuples;
 
 		/*
 		 * Note that we leave rel->pages as zero; this is important to avoid

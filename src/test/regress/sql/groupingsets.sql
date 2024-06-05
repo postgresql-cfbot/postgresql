7052ce41e4c4cce613b98534836255e74c197c83
Richard Guo <guofenglinux@gmail.com>
2024-06-05 10:32:10 +0900

    Mark expressions nullable by grouping sets
    When generating window_pathkeys, distinct_pathkeys, or sort_pathkeys,
    we failed to realize that the grouping/ordering expressions might be
    nullable by grouping sets.  As a result, we may incorrectly deem that
    the PathKeys are redundant by EquivalenceClass processing and thus
    remove them from the pathkeys list.  That would lead to wrong results
    in some cases.
    
    To fix this issue, we mark the grouping expressions nullable by
    grouping sets if that is the case.  If the grouping expression is a
    Var or PlaceHolderVar or constructed from those, we can just add the
    RT index of the RTE_GROUP RTE to the existing nullingrels field(s);
    otherwise we have to add a PlaceHolderVar to carry on the nullingrel
    bit.
    
    However, we have to manually remove this nullingrel bit from
    expressions in various cases where these expressions are logically
    below the grouping step, such as when we generate groupClause pathkeys
    for grouping sets, or when we generate PathTarget for initial input to
    grouping nodes.
    
    Furthermore, in set_upper_references, the targetlist and quals of an
    Agg node should have nullingrels that include the effects of the
    grouping step, ie they will have nullingrels equal to the input
    Vars/PHVs' nullingrels plus the nullingrel bit that references the
    grouping RTE.  In order to perform exact nullingrels matches, we also
    need to manually remove this nullingrel bit.

--- src/test/regress/sql/groupingsets.sql
+++ src/test/regress/sql/groupingsets.sql
@@ -631,4 +631,51 @@ from gstest5 t1
 group by grouping sets(v, s)
 order by o nulls first;
 
+-- test handling of expressions nullable by grouping sets
+explain (costs off)
+select distinct on (a, b) a, b
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a))
+order by a, b;
+
+select distinct on (a, b) a, b
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a))
+order by a, b;
+
+explain (costs off)
+select distinct on (a, b+1) a, b+1
+from (values (1, 0), (2, 1)) as t (a, b) where a = b+1
+group by grouping sets((a, b+1), (a))
+order by a, b+1;
+
+select distinct on (a, b+1) a, b+1
+from (values (1, 0), (2, 1)) as t (a, b) where a = b+1
+group by grouping sets((a, b+1), (a))
+order by a, b+1;
+
+explain (costs off)
+select a, b
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a))
+order by a, b nulls first;
+
+select a, b
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a))
+order by a, b nulls first;
+
+explain (costs off)
+select 1 as one group by rollup(one) order by one nulls first;
+select 1 as one group by rollup(one) order by one nulls first;
+
+explain (costs off)
+select a, b, row_number() over (order by a, b nulls first)
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a));
+
+select a, b, row_number() over (order by a, b nulls first)
+from (values (1, 1), (2, 2)) as t (a, b) where a = b
+group by grouping sets((a, b), (a));
+
 -- end

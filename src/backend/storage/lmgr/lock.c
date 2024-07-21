923cd55a1604956da80d644cbbe240f53bf0ba98
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-07-22 01:21:20 +0300

    Remove LOCK_PRINT() call that could point to garbage
    If a deadlock is detected in ProcSleep, it removes the process from
    the wait queue and the shared LOCK and PROCLOCK entries. As soon as it
    does that, the other process holding the lock might release it, and
    remove the LOCK entry altogether. So on return from ProcSleep(), it's
    possible that the LOCK entry is no longer valid.

--- src/backend/storage/lmgr/lock.c
+++ src/backend/storage/lmgr/lock.c
@@ -1910,8 +1910,6 @@ WaitOnLock(LOCALLOCK *locallock, ResourceOwner owner, bool dontWait)
 			 * now.
 			 */
 			awaitedLock = NULL;
-			LOCK_PRINT("WaitOnLock: aborting on lock",
-					   locallock->lock, locallock->tag.mode);
 			LWLockRelease(LockHashPartitionLock(locallock->hashcode));
 
 			/*

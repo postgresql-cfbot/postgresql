021a77f0cded4173e4e5c13950e8109838028a29
Sami Imseih <simseih@amazon.com>
2024-08-12 15:47:55 -0500

    vaccum_delay with absolute time nanosleep

--- src/port/pgsleep.c
+++ src/port/pgsleep.c
@@ -14,6 +14,8 @@
 
 #include <time.h>
 
+#include "portability/instr_time.h"
+
 /*
  * In a Windows backend, we don't use this implementation, but rather
  * the signal-aware version in src/backend/port/win32/signal.c.
@@ -53,5 +55,52 @@ pg_usleep(long microsec)
 #endif
 	}
 }
+
+/*
+ * pg_usleep_non_interruptible --- delay the specified number of microseconds.
+ *
+ * Unlike pg_usleep, this function continues the delay in case of an
+ * interrupt.
+ */
+void
+pg_usleep_non_interruptible(long microsec)
+{
+	/*
+	 * We allow nanosleep to handle interrupts and retry with the remaining
+	 * time. However, frequent interruptions and restarts of the nanosleep
+	 * calls can substantially lead to drift in the time when the sleep
+	 * finally completes. To deal with this, we break out of the loop whenever
+	 * the current time is past the expected end time of the sleep.
+	 */
+	if (microsec > 0)
+	{
+#ifndef WIN32
+		struct timespec delay;
+		struct timespec remain;
+		instr_time	end_time;
+
+		INSTR_TIME_SET_CURRENT(end_time);
+		INSTR_TIME_ADD_MICROSEC(end_time, microsec);
+
+		delay.tv_sec = microsec / 1000000L;
+		delay.tv_nsec = (microsec % 1000000L) * 1000;
+
+		while (nanosleep(&delay, &remain) == -1 && errno == EINTR)
+		{
+			instr_time	current_time;
+
+			INSTR_TIME_SET_CURRENT(current_time);
+
+			if (INSTR_TIME_IS_GREATER(current_time, end_time))
+				break;
+
+			delay = remain;
+		}
+#else
+		SleepEx((microsec < 500 ? 1 : (microsec + 500) / 1000), FALSE);
+#endif
+	}
+}
+
 
 #endif							/* defined(FRONTEND) || !defined(WIN32) */

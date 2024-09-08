18def396ca722cf54acc6603f0bbb22766b029a5
Stepan Neretin <sndcppg@gmail.com>
2024-09-08 15:44:10 +0700

    Introduce a sorting template for float8 arrays in geo_spgist.c to boost performance.

--- src/backend/utils/adt/geo_spgist.c
+++ src/backend/utils/adt/geo_spgist.c
@@ -100,6 +100,17 @@ compareDoubles(const void *a, const void *b)
 	return (x > y) ? 1 : -1;
 }
 
+ /*
++ * Instantiating a Sorting Template for float8 Arrays
++ * enhancing speed performance.
++ */
+#define ST_SORT sort_float8_arr
+#define ST_ELEMENT_TYPE float8
+#define ST_COMPARE(a, b) compareDoubles(a, b)
+#define ST_SCOPE static
+#define ST_DEFINE
+#include <lib/sort_template.h>
+
 typedef struct
 {
 	float8		low;

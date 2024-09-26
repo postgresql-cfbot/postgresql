10fc1ca7399ba64aa0c32d8b607043567315cb51
Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
2024-09-26 12:06:25 +0530

    RestrictInfo hash table interface
    Introduces the RestrictInfo hash table interface and adds a pointer to
    RestrictInfo hash table in PlannerInfo. RestrictInfo::rinfo_serial and
    RestrictInfo::required_relids are used as key into the hash table.
    
    This commit does not add any code which uses the interface itself.
    Adding a new member to PlannerInfo increases it size and may have slight
    impact due to cacheline faults when accessing this huge structure.
    Similarly the interface code increases object file size which again may
    have some impact on performance. This is a separate commit to assess any
    performance or memory impact this change has.
    
    Ashutosh Bapat

--- src/include/optimizer/restrictinfo.h
+++ src/include/optimizer/restrictinfo.h
@@ -47,6 +47,9 @@ extern bool join_clause_is_movable_to(RestrictInfo *rinfo, RelOptInfo *baserel);
 extern bool join_clause_is_movable_into(RestrictInfo *rinfo,
 										Relids currentrelids,
 										Relids current_and_outer);
+extern RestrictInfo *find_restrictinfo(PlannerInfo *root, int rinfo_serial,
+									   Relids child_required_relids);
+extern void add_restrictinfo(PlannerInfo *root, RestrictInfo *child_rinfo);
 
 /*
  * clause_sides_match_join

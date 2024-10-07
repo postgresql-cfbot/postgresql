b97c2ddd528022df147ce4ed4e249d1c79661c04
Atsushi Torikoshi <torikoshia@oss.nttdata.com>
2024-10-07 19:57:51 +0900

    Subject: Add REJECT_LIMIT option to COPY command.
    9e2d870 enabled the COPY FROM command to skip soft errors, but there
    was no limitation about the number of errors and skipped all the soft
    errors.
    In some cases there would be a limitation how many errors are
    tolerable. This patch introduces REJECT_LIMIT to specify how many
    errors can be skipped.

--- src/backend/commands/copyfrom.c
+++ src/backend/commands/copyfrom.c
@@ -1018,6 +1018,15 @@ CopyFrom(CopyFromState cstate)
 			pgstat_progress_update_param(PROGRESS_COPY_TUPLES_SKIPPED,
 										 cstate->num_errors);
 
+			if (cstate->opts.reject_limit > 0 && \
+				cstate->num_errors > cstate->opts.reject_limit)
+				ereport(ERROR,
+						(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+						 errmsg_plural("skipped more than REJECT_LIMIT (%lld) row due to data type incompatibility",
+									   "skipped more than REJECT_LIMIT (%lld) rows due to data type incompatibility",
+									   (long long) cstate->opts.reject_limit,
+									   (long long) cstate->opts.reject_limit)));
+
 			/* Repeat NextCopyFrom() until no soft error occurs */
 			continue;
 		}

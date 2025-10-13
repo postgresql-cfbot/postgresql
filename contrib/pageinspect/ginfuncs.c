4679743872ad87a3c90290b1d6b9917fd05dc26d
reshke <reshke@double.cloud>
2025-10-13 20:14:26 +0000

    GIN pageinspect support for entry tree and posting tree internal pages
    This patch provides a new version for the pageinspect contrib module, including
    two new functions:
    
    * gin_entrypage_items.
    
    * gin_datapage_items.
    
    These two functions can be used to examine the GIN entry tree and posting
    tree pages. Namely, gin_entrypage_items can be used for both leaf and
    non-leaf entry tree pages. gin_datapage_items is provided in pairs with
    already-existing gin_leafpage_items to examine the non-leaf posting tree pages.
    
    We keep the different functions here mainly because of different GIN.
    pages layoff.
    
    Note that fast-list pages are out of scope of this patch.
    
    Co-authored-by: Peter Eisentraut <peter@eisentraut.org>
    Reviewed-by: Andrey Borodin <x4mmm@yandex-team.ru>
    Reviewed-by: Roman Khapov <rkhapov@yandex-team.ru>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Reviewed-by: Japin Li <japinli@hotmail.com>
    
    Discussion: https://postgr.es/m/CALdSSPiN13n7feQcY0WCmq8jzxjwqhNrt1E=g=g6aZANyE_OoQ@mail.gmail.com

--- contrib/pageinspect/ginfuncs.c
+++ contrib/pageinspect/ginfuncs.c
@@ -11,18 +11,28 @@
 
 #include "access/gin_private.h"
 #include "access/htup_details.h"
+#include "access/relation.h"
+#include "access/tupdesc.h"
 #include "catalog/pg_type.h"
 #include "funcapi.h"
 #include "miscadmin.h"
 #include "pageinspect.h"
 #include "utils/array.h"
 #include "utils/builtins.h"
+#include "utils/lsyscache.h"
+#include "utils/rel.h"
+#include "utils/ruleutils.h"
+#include "utils/tuplestore.h"
 
 
 PG_FUNCTION_INFO_V1(gin_metapage_info);
 PG_FUNCTION_INFO_V1(gin_page_opaque_info);
+PG_FUNCTION_INFO_V1(gin_entrypage_items);
 PG_FUNCTION_INFO_V1(gin_leafpage_items);
+PG_FUNCTION_INFO_V1(gin_datapage_items);
 
+#define IS_INDEX(r) ((r)->rd_rel->relkind == RELKIND_INDEX)
+#define IS_GIN(r) ((r)->rd_rel->relam == GIN_AM_OID)
 
 Datum
 gin_metapage_info(PG_FUNCTION_ARGS)
@@ -174,6 +184,340 @@ typedef struct gin_leafpage_items_state
 	GinPostingList *seg;
 	GinPostingList *lastseg;
 } gin_leafpage_items_state;
+
+/*
+ * gin_entrypage_items
+ *
+ * Allows inspection of contents of an entry tree page.
+ */
+Datum
+gin_entrypage_items(PG_FUNCTION_ARGS)
+{
+	bytea	   *raw_page = PG_GETARG_BYTEA_P(0);
+	Oid			indexRelid = PG_GETARG_OID(1);
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	Relation	indexRel;
+	OffsetNumber maxoff;
+	TupleDesc	tupdesc;
+	Page		page;
+	GinPageOpaque opaq;
+	StringInfoData buf;
+
+	if (!superuser())
+		ereport(ERROR,
+				errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				errmsg("must be superuser to use raw page functions"));
+
+	InitMaterializedSRF(fcinfo, 0);
+
+	/* Open the index relation */
+	indexRel = index_open(indexRelid, AccessShareLock);
+
+	if (!IS_INDEX(indexRel) || !IS_GIN(indexRel))
+		ereport(ERROR,
+				errcode(ERRCODE_WRONG_OBJECT_TYPE),
+				errmsg("\"%s\" is not a %s index",
+					   RelationGetRelationName(indexRel), "GIN"));
+
+	page = get_page_from_raw(raw_page);
+
+	if (PageIsNew(page))
+	{
+		index_close(indexRel, AccessShareLock);
+		PG_RETURN_NULL();
+	}
+
+	if (PageGetSpecialSize(page) != MAXALIGN(sizeof(GinPageOpaqueData)))
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("input page is not a valid GIN entry tree page"),
+				errdetail("Expected special size %d, got %d.",
+						  (int) MAXALIGN(sizeof(GinPageOpaqueData)),
+						  (int) PageGetSpecialSize(page)));
+
+	opaq = GinPageGetOpaque(page);
+
+	/* we only support entry tree in this function, check that */
+	if (opaq->flags & GIN_META)
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_entrypage_items does not support metapages"));
+
+	if (opaq->flags & (GIN_LIST | GIN_LIST_FULLROW))
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_entrypage_items does not support fast list pages"));
+
+	if (opaq->flags & GIN_DATA)
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_entrypage_items does not support posting tree pages"),
+				errhint("This appears to be a GIN posting tree page. Please use gin_datapage_items."));
+
+	initStringInfo(&buf);
+	maxoff = PageGetMaxOffsetNumber(page);
+
+	tupdesc = RelationGetDescr(indexRel);
+
+	for (OffsetNumber offset = FirstOffsetNumber;
+		 offset <= maxoff;
+		 offset = OffsetNumberNext(offset))
+	{
+		OffsetNumber indAtt;
+		Datum		values[4];
+		bool		nulls[4] = {0};
+		Datum		attrVal;
+		bool		isnull;
+		IndexTuple	idxtuple;
+		ItemId		iid = PageGetItemId(page, offset);
+
+		if (!ItemIdIsValid(iid))
+			ereport(ERROR, errcode(ERRCODE_INDEX_CORRUPTED), errmsg("invalid ItemId at offset %u", offset));
+
+		idxtuple = (IndexTuple) PageGetItem(page, iid);
+
+		values[0] = UInt16GetDatum(offset);
+
+		if (tupdesc->natts == 1)
+		{
+			indAtt = FirstOffsetNumber;
+
+			/* Here we can safely reuse any tuple descriptor. */
+			attrVal = index_getattr(idxtuple, FirstOffsetNumber, tupdesc, &isnull);
+		}
+		else
+		{
+			TupleDesc	tmpTupdesc;
+			Datum		res;
+			Form_pg_attribute attr;
+
+			/*
+			 * Multi-column GIN indexes store 2-attribute tuple on each page
+			 * item. First attribute is which heap attribute is stored as the
+			 * second value in pair. To display value with proper output
+			 * function we need to recreate tuple descriptor on each offset.
+			 * NB: It is safe to reuse the original index tuple. See also
+			 * gintuple_get_attrnum.
+			 */
+
+			res = index_getattr(idxtuple, FirstOffsetNumber, tupdesc, &isnull);
+
+			/*
+			 * we do not expect null for first attr in multi-column GIN
+			 */
+			if (isnull)
+				ereport(ERROR,
+						errcode(ERRCODE_INDEX_CORRUPTED),
+						errmsg("invalid gin entry page tuple at offset %u", offset));
+
+			indAtt = DatumGetUInt16(res);
+
+			attr = TupleDescAttr(tupdesc, indAtt - 1);
+
+			tmpTupdesc = CreateTemplateTupleDesc(2);
+
+			TupleDescInitEntry(tmpTupdesc, (AttrNumber) 1, NULL,
+							   INT2OID, -1, 0);
+			TupleDescInitEntry(tmpTupdesc, (AttrNumber) 2, NULL,
+							   attr->atttypid,
+							   attr->atttypmod,
+							   attr->attndims);
+			TupleDescInitEntryCollation(tmpTupdesc, (AttrNumber) 2,
+										attr->attcollation);
+
+			attrVal = index_getattr(idxtuple, OffsetNumberNext(FirstOffsetNumber),
+									tmpTupdesc,
+									&isnull);
+
+			FreeTupleDesc(tmpTupdesc);
+		}
+
+		appendStringInfo(&buf, "%s=", quote_identifier(TupleDescAttr(tupdesc, indAtt - 1)->attname.data));
+
+		if (!isnull)
+		{
+			Oid			foutoid;
+			bool		typisvarlena;
+			Oid			typoid;
+			char	   *value;
+			bool		nq;
+
+			/*
+			 * The following value output and quoting logic is copied from
+			 * record_out().
+			 */
+			typoid = TupleDescAttr(tupdesc, indAtt - 1)->atttypid;
+			getTypeOutputInfo(typoid, &foutoid, &typisvarlena);
+			value = OidOutputFunctionCall(foutoid, attrVal);
+
+			/* Check whether we need double quotes for this value */
+			nq = (value[0] == '\0');	/* force quotes for empty string */
+			for (const char *tmp = value; *tmp; tmp++)
+			{
+				char		ch = *tmp;
+
+				if (ch == '"' || ch == '\\' ||
+					ch == '(' || ch == ')' || ch == ',' ||
+					isspace((unsigned char) ch))
+				{
+					nq = true;
+					break;
+				}
+			}
+
+			/* And emit the string */
+			if (nq)
+				appendStringInfoCharMacro(&buf, '"');
+			for (const char *tmp = value; *tmp; tmp++)
+			{
+				char		ch = *tmp;
+
+				if (ch == '"' || ch == '\\')
+					appendStringInfoCharMacro(&buf, ch);
+				appendStringInfoCharMacro(&buf, ch);
+			}
+			if (nq)
+				appendStringInfoCharMacro(&buf, '"');
+		}
+		else
+		{
+			appendStringInfo(&buf, "NULL");
+		}
+
+		values[3] = CStringGetTextDatum(buf.data);
+		resetStringInfo(&buf);
+
+		if (GinIsPostingTree(idxtuple))
+		{
+			values[1] = ItemPointerGetDatum(&idxtuple->t_tid);
+			nulls[2] = true;
+		}
+		else
+		{
+			int			ndecoded;
+			Datum	   *tids_datum;
+			ItemPointer items_orig;
+			bool		free_items_orig;
+
+			values[1] = ItemPointerGetDatum(&idxtuple->t_tid);
+			/* Get list of item pointers from the tuple. */
+			if (GinItupIsCompressed(idxtuple))
+			{
+				items_orig = ginPostingListDecode((GinPostingList *) GinGetPosting(idxtuple), &ndecoded);
+				free_items_orig = true;
+			}
+			else
+			{
+				items_orig = (ItemPointer) GinGetPosting(idxtuple);
+				ndecoded = GinGetNPosting(idxtuple);
+				free_items_orig = false;
+			}
+
+			tids_datum = palloc_array(Datum, ndecoded);
+			for (int i = 0; i < ndecoded; i++)
+				tids_datum[i] = ItemPointerGetDatum(&items_orig[i]);
+			values[2] = PointerGetDatum(construct_array_builtin(tids_datum, ndecoded, TIDOID));
+
+			pfree(tids_datum);
+
+			if (free_items_orig)
+				pfree(items_orig);
+		}
+
+		/* Build and return the result tuple. */
+		tuplestore_putvalues(rsinfo->setResult, rsinfo->setDesc, values, nulls);
+	}
+
+	index_close(indexRel, AccessShareLock);
+
+	return (Datum) 0;
+}
+
+/*
+ * gin_datapage_items
+ *
+ * Allows inspection of contents of an posting tree non-leaf page.
+ */
+Datum
+gin_datapage_items(PG_FUNCTION_ARGS)
+{
+	bytea	   *raw_page = PG_GETARG_BYTEA_P(0);
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	OffsetNumber maxoff;
+	Page		page;
+	GinPageOpaque opaq;
+
+	if (!superuser())
+		ereport(ERROR,
+				errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				errmsg("must be superuser to use raw page functions"));
+
+	InitMaterializedSRF(fcinfo, 0);
+	page = get_page_from_raw(raw_page);
+
+	if (PageIsNew(page))
+	{
+		PG_RETURN_NULL();
+	}
+
+	if (PageGetSpecialSize(page) != MAXALIGN(sizeof(GinPageOpaqueData)))
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("input page is not a valid GIN data page"),
+				errdetail("Expected special size %d, got %d.",
+						  (int) MAXALIGN(sizeof(GinPageOpaqueData)),
+						  (int) PageGetSpecialSize(page)));
+
+	opaq = GinPageGetOpaque(page);
+
+	/*
+	 * Reject non-posting-tree-internal GIN pages, which are the metapage, fast
+	 * list pages, entry tree pages and posting tree leaf pages.
+	 */
+	if (opaq->flags & GIN_META)
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_datapage_items does not support metapages"));
+
+	if (opaq->flags & (GIN_LIST | GIN_LIST_FULLROW))
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_datapage_items does not support fast list pages"));
+
+	if (!(opaq->flags & GIN_DATA))
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("input page is not a GIN data tree page"));
+
+	if (opaq->flags & GIN_LEAF)
+		ereport(ERROR,
+				errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("gin_datapage_items does not support posting tree leaf pages"),
+				errhint("This appears to be a GIN posting tree leaf page. Please use gin_leafpage_items."));
+
+	maxoff = opaq->maxoff;
+
+	for (OffsetNumber offset = FirstOffsetNumber;
+		 offset <= maxoff;
+		 offset = OffsetNumberNext(offset))
+	{
+		Datum		values[3];
+		bool		nulls[3];
+		PostingItem *item = GinDataPageGetPostingItem(page, offset);
+
+		memset(nulls, 0, sizeof(nulls));
+
+		values[0] = UInt16GetDatum(offset);
+
+		values[1] = UInt32GetDatum(BlockIdGetBlockNumber(&item->child_blkno));
+		values[2] = ItemPointerGetDatum(&item->key);
+
+		/* Build and return the result tuple. */
+		tuplestore_putvalues(rsinfo->setResult, rsinfo->setDesc, values, nulls);
+	}
+
+	return (Datum) 0;
+}
 
 Datum
 gin_leafpage_items(PG_FUNCTION_ARGS)

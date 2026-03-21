17117b159ba837ded57690802c1cc9fc0acfb3ee
Lukas Fittl <lukas@fittl.com>
2026-03-21 11:02:37 -0700

    Allow resource owners to access subsidiary portal memory during cleanup
    During AbortTransaction/AbortSubTransaction we free portal child memory
    contexts, aka subsidiary contexts, ahead of freeing the portal memory
    itself, in order to not keep short-lived contexts like the executor
    context around longer than needed, e.g. in the case of holdable cursors.
    
    However, we currently free such memory when we first mark the portal
    as aborted, and before calling ResourceOwnerRelease. This presents a
    challenge for any resource owner that wants to access memory that was
    allocated inside the portal (e.g. with the executor context) and is
    registered to a resource owner that wants to use it during clean up. The
    current workaround is to allocate such memory in the TopMemoryContext,
    and use precise pfree(..) calls in both abort and success cases to avoid
    leaks.
    
    Instead, separate marking portals as aborted (and calling the cleanup hook)
    from releasing subsidiary memory contexts. Delay releasing the memory
    contexts until ResourceOwnerRelease has been called for all phases. For
    example, this allows allocating memory in the per-resource context and
    then accessing it when cleaning up a resource.
    
    Since abort handling is not supposed to allocate new memory (the
    pre-allocated TransactionAbortContext exists for that purpose), this
    should have limited performance impact in practice, and will allow both
    potential refactorings of current resource owners, and avoid complexity
    in future patches.
    
    Author: Lukas Fittl <lukas@fittl.com>
    Reviewed By:
    Discussion:

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3030,6 +3030,15 @@ AbortTransaction(void)
 		ResourceOwnerRelease(TopTransactionResourceOwner,
 							 RESOURCE_RELEASE_AFTER_LOCKS,
 							 false, true);
+
+		/*
+		 * Now that resource owner cleanup has finished, we can release the
+		 * memory resource owners might have still accessed during cleanup.
+		 *
+		 * Note the portal context itself is freed later in PortalDrop.
+		 */
+		AtAbort_Portals_ReleaseMemory();
+
 		smgrDoPendingDeletes(false);
 
 		AtEOXact_GUC(false, 1);
@@ -3047,6 +3056,10 @@ AbortTransaction(void)
 		AtEOXact_LogicalRepWorkers(false);
 		AtEOXact_LogicalCtl();
 		pgstat_report_xact_timestamp(0);
+	}
+	else
+	{
+		AtAbort_Portals_ReleaseMemory();
 	}
 
 	/*
@@ -4970,6 +4983,7 @@ AbortOutOfAnyTransaction(void)
 				 * we need to shut down before doing CleanupTransaction.
 				 */
 				AtAbort_Portals();
+				AtAbort_Portals_ReleaseMemory();
 				CleanupTransaction();
 				s->blockState = TBLOCK_DEFAULT;
 				break;
@@ -4999,6 +5013,7 @@ AbortOutOfAnyTransaction(void)
 									   s->parent->subTransactionId,
 									   s->curTransactionOwner,
 									   s->parent->curTransactionOwner);
+					AtSubAbort_Portals_ReleaseMemory(s->subTransactionId);
 				}
 				CleanupSubTransaction();
 				s = CurrentTransactionState;	/* changed by pop */
@@ -5407,6 +5422,15 @@ AbortSubTransaction(void)
 		ResourceOwnerRelease(s->curTransactionOwner,
 							 RESOURCE_RELEASE_AFTER_LOCKS,
 							 false, false);
+
+		/*
+		 * Now that resource owner cleanup has finished, we can release the
+		 * memory resource owners might have still accessed during cleanup.
+		 *
+		 * Note the portal context itself is freed later in PortalDrop.
+		 */
+		AtSubAbort_Portals_ReleaseMemory(s->subTransactionId);
+
 		AtSubAbort_smgr();
 
 		AtEOXact_GUC(false, s->gucNestLevel);

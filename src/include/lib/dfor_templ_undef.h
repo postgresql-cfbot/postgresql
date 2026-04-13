b0aeb66ed5d3d2e15d5e955f2cf2472e605a7230
Evgeny Voropaev <evorop@gmail.com>
2026-04-13 23:15:34 +0800

    Implement Delta Frame of Reference compression.
    Implement the compression algorithm based on the Delta Frame of
    Reference technique (DFOR).
    
    The current implementation of the DFOR algorithm supports items of
    unsigned integer types. The type of an item is defined with the
    item_t macro.
    
    The compressed pack comprises three parts:
        - deltas;
        - exceptions positions.
        - exceptions;
    Each of them is a densily bit-packed sequence.
    
    The delta is a difference between the current item and the previous one. The
    delta of the first item (the item having the zero index) is its actual value:
    delta[0] = m[0]-0 = m[0]. Serialised deltas is a sequence of bits. Each
    serialised delta in 'deltas' has a fixed bit width. If the delta's width
    exceeds the allowed size of a delta in 'deltas', the higher bits of this
    delta is put into exceptions, its positions is saved into the 'excepton
    positions' section. Usage of exceptions can be deliberately turned off.
    
    DFoR supports both external memory (outer memory) provided by a caller
    and automatically managed memory, allocated by means of malloc, palloc
    or similar functions. Memory management configuration must be defined
    during initialization. All subsequent operations follow this
    configuration. For example, a caller can place a buffer on the stack to
    avoid heap allocation and pass the buffer to a DFoR unit. As a result,
    the packing and unpacking processes exclude dynamic allocation.
    
    The DFoR unit is implemented as a set of templates. Developers can
    generate DFoR implementations for any unsigned integer type (uint8_t,
    uint16_t, uint32_t, uint64_t). The dfor_u16 unit is implemented.
    
    Author: Evgeny Voropaev <evgeny.voropaev@tantorlabs.com> <evorop@gmail.com>
    Reviewed by: Andrey Borodin <x4mmm@yandex-team.ru>

---
+++ src/include/lib/dfor_templ_undef.h
@@ -0,0 +1,28 @@
+#undef item_t
+#undef vect_t
+#undef uniqsortvect_t
+
+#undef dfor_calc_deltas
+#undef dfor_calc_width
+#undef dfor_pack
+#undef dfor_unpack
+#undef dfor_analyze
+#undef dfor_calc_stats
+#undef dfor_calc_nbytes
+
+#undef vect_init
+#undef vect_fill
+#undef vect_reserve
+#undef vect_append
+#undef vect_print
+#undef vect_compare
+#undef vect_insert
+#undef vect_clear
+
+#undef usv_insert
+#undef usv_search
+
+#undef width_from_val
+#undef width_to_mask
+#undef bitpack_pack
+#undef bitpack_unpack

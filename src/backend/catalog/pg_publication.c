2eedd2d779bce9536088f4f011a3e4ccbea9e8d2
Peter Smith <peter.b.smith@fujitsu.com>
2026-06-03 15:02:55 +1000

    fix errmsg grammar for publication membership

--- src/backend/catalog/pg_publication.c
+++ src/backend/catalog/pg_publication.c
@@ -555,7 +555,7 @@ publication_add_relation(Oid pubid, PublicationRelInfo *pri,
 
 		ereport(ERROR,
 				(errcode(ERRCODE_DUPLICATE_OBJECT),
-				 errmsg("relation \"%s\" is already member of publication \"%s\"",
+				 errmsg("relation \"%s\" is already a member of publication \"%s\"",
 						RelationGetRelationName(targetrel), pub->name)));
 	}
 
@@ -816,7 +816,7 @@ publication_add_schema(Oid pubid, Oid schemaid, bool if_not_exists)
 
 		ereport(ERROR,
 				(errcode(ERRCODE_DUPLICATE_OBJECT),
-				 errmsg("schema \"%s\" is already member of publication \"%s\"",
+				 errmsg("schema \"%s\" is already a member of publication \"%s\"",
 						get_namespace_name(schemaid), pub->name)));
 	}
 

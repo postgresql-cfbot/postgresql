tristan957/postgres

Fix mismatched deallocation functions
In fe_memutils.h, we have various allocation functions beginning with
either pg_ or p. The pg_ functions have a matching pg_free() for freeing
memory, while the p functions use pfree(). In some cases, we were
allocating memory with one set of functions while using the wrong
deallocation functions. This creates a tiny bit of mental overhead when
reading code. Matching up allocation and deallocation functions makes it
easier to analyze memory handling in a code path.

src/bin/psql/startup.c
@@ -276,8 +276,8 @@ main(int argc, char *argv[])
 
 		new_pass = false;
 		pset.db = PQconnectdbParams(keywords, values, true);
-		free(keywords);
-		free(values);
+		pg_free(keywords);
+		pg_free(values);
 
 		if (PQstatus(pset.db) == CONNECTION_BAD &&
 			PQconnectionNeedsPassword(pset.db) &&
@@ -300,7 +300,7 @@ main(int argc, char *argv[])
 			PQfinish(pset.db);
 
 			password = simple_prompt(password_prompt, false);
-			free(password_prompt);
+			pg_free(password_prompt);
 			new_pass = true;
 		}
 	} while (new_pass);
@@ -620,7 +620,7 @@ parse_psql_options(int argc, char *argv[], struct adhoc_opts *options)
 					if (!result)
 						pg_fatal("could not set printing parameter \"%s\"", value);
 
-					free(value);
+					pg_free(value);
 					break;
 				}
 			case 'q':
@@ -664,7 +664,7 @@ parse_psql_options(int argc, char *argv[], struct adhoc_opts *options)
 							exit(EXIT_FAILURE); /* error already printed */
 					}
 
-					free(value);
+					pg_free(value);
 					break;
 				}
 			case 'V':
@@ -834,8 +834,8 @@ process_psqlrc_file(char *filename)
 	else if (access(filename, R_OK) == 0)
 		(void) process_file(filename, false);
 
-	free(psqlrc_minor);
-	free(psqlrc_major);
+	pfree(psqlrc_minor);
+	pfree(psqlrc_major);
 }
 
 

postgresql-cfbot/postgresql

Signed-off-by: Tristan Partin <tristan@partin.io>

src/bin/psql/startup.c
@@ -276,8 +276,8 @@ main(int argc, char *argv[])
 
 		new_pass = false;
 		pset.db = PQconnectdbParams(keywords, values, true);
-		free(keywords);
-		free(values);
+		pg_free(keywords);
+		pg_free(values);
 
 		if (PQstatus(pset.db) == CONNECTION_BAD &&
 			PQconnectionNeedsPassword(pset.db) &&
@@ -300,7 +300,7 @@ main(int argc, char *argv[])
 			PQfinish(pset.db);
 
 			password = simple_prompt(password_prompt, false);
-			free(password_prompt);
+			pfree(password_prompt);
 			new_pass = true;
 		}
 	} while (new_pass);
@@ -620,7 +620,7 @@ parse_psql_options(int argc, char *argv[], struct adhoc_opts *options)
 					if (!result)
 						pg_fatal("could not set printing parameter \"%s\"", value);
 
-					free(value);
+					pg_free(value);
 					break;
 				}
 			case 'q':
@@ -664,7 +664,7 @@ parse_psql_options(int argc, char *argv[], struct adhoc_opts *options)
 							exit(EXIT_FAILURE); /* error already printed */
 					}
 
-					free(value);
+					pg_free(value);
 					break;
 				}
 			case 'V':
@@ -834,8 +834,8 @@ process_psqlrc_file(char *filename)
 	else if (access(filename, R_OK) == 0)
 		(void) process_file(filename, false);
 
-	free(psqlrc_minor);
-	free(psqlrc_major);
+	pfree(psqlrc_minor);
+	pfree(psqlrc_major);
 }
 
 
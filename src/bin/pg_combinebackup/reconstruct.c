71d95d0cc03f0f450388b708c229bee24c6d99e4
Peter Eisentraut <peter@eisentraut.org>
2026-06-29 13:37:54 +0200

    Clean up copy_file_range() return type
    The return type is ssize_t, not int.  Fix that in one instance; the
    others were already okay.

--- src/bin/pg_combinebackup/reconstruct.c
+++ src/bin/pg_combinebackup/reconstruct.c
@@ -698,7 +698,7 @@ write_reconstructed_file(const char *input_filename,
 			 */
 			do
 			{
-				int			wb;
+				ssize_t		wb;
 
 				wb = copy_file_range(s->fd, &off, wfd, NULL, BLCKSZ - nwritten, 0);
 

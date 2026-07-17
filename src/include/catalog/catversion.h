a32733d8f10f0e35f7e4ee772ddbef552296577f
Tom Lane <tgl@sss.pgh.pa.us>
2026-07-17 16:13:57 -0400

    Revert "Add GROUP BY ALL".
    This reverts commit ef38a4d9756db9ae1d20f40aa39f3cf76059b81a which
    implemented the GROUP BY ALL syntax, as well as 2ce745836 which
    made some comment improvements therein.
    
    A postcommit review discovered that GROUP BY ALL missed our special
    handling of entries that also appear in an ORDER BY in the query.
    This caused the query to return wrong results when ORDER BY
    specifies non-default equality semantics.  While this should be
    fixable with some refactoring, doing it cleanly seems like too
    much code churn for late beta.  We'll revert and try again in v20.
    
    The reverted commit also included some additional comment wordsmithing
    and docs cleanup, which are retained as they weren't connected to the
    reverted feature.
    
    catversion bump needed due to change in struct Query.
    
    Reported-by: Chao Li <li.evan.chao@gmail.com>
    Author: Daniel Gustafsson <daniel@yesql.se>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/5243308F-8E5C-45AA-828C-FAD96C4F34DA@gmail.com
    Backpatch-through: 19

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -57,6 +57,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	202607171
+#define CATALOG_VERSION_NO	202607173
 
 #endif
--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -220,7 +220,6 @@ typedef struct Query
 
 	List	   *groupClause;	/* a list of SortGroupClause's */
 	bool		groupDistinct;	/* was GROUP BY DISTINCT used? */
-	bool		groupByAll;		/* was GROUP BY ALL used? */
 
 	List	   *groupingSets;	/* a list of GroupingSet's if present */
 
@@ -2341,7 +2340,6 @@ typedef struct SelectStmt
 	Node	   *whereClause;	/* WHERE qualification */
 	List	   *groupClause;	/* GROUP BY clauses */
 	bool		groupDistinct;	/* Is this GROUP BY DISTINCT? */
-	bool		groupByAll;		/* Is this GROUP BY ALL? */
 	Node	   *havingClause;	/* HAVING conditional-expression */
 	List	   *windowClause;	/* WINDOW window_name AS (...), ... */
 

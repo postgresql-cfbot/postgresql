hackorum-dev/postgres

Add support for INSERT ... SET syntax
This commit adds support for INSERT ... SET syntax, which allows
specifying column values using named assignments instead of
requiring a separate column list and VALUES clause.

Syntax:
    INSERT INTO table_name SET column1=value1, column2=value2, ...;

This syntax provides a more convenient and readable alternative for
single-row inserts, particularly when only specific columns need values.
Columns not mentioned in the SET clause receive their default values or
NULL, consistent with standard INSERT behavior.

Features supported:
- Basic syntax: INSERT INTO t SET col=val, ...
- DEFAULT keyword: SET col=DEFAULT
- NULL values: SET col=NULL
- Expressions and functions: SET col=expr
- Subqueries: SET col=(SELECT ...)
- RETURNING clause: SET ... RETURNING *
- ON CONFLICT: SET ... ON CONFLICT DO UPDATE/NOTHING
- OVERRIDING SYSTEM VALUE: OVERRIDING SYSTEM VALUE SET ...
- Multi-row syntax: SET (col1=val1, col2=val2), (col1=val3, col2=val4)
- Support for different column sets in multi-row inserts

src/backend/parser/gram.y
@@ -562,6 +562,8 @@ static Node *makeRecursiveViewSelect(char *relname, List *aliases, Node *query);
 %type <node>	for_portion_of_clause
 %type <node>	tablesample_clause opt_repeatable_clause
 %type <target>	target_el set_target insert_column_item
+%type <list>	insert_set_clause insert_set_clause_list
+%type <list>	insert_set_clause_group insert_set_clause_group_list
 
 %type <str>		generic_option_name
 %type <node>	generic_option_arg
@@ -13068,6 +13070,36 @@ insert_rest:
 					$$->cols = NIL;
 					$$->selectStmt = NULL;
 				}
+			| OVERRIDING override_kind VALUE_P SET insert_set_clause_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->override = $2;
+					$$->setClauseList = list_make1($5);
+				}
+			| OVERRIDING override_kind VALUE_P SET insert_set_clause_group_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->override = $2;
+					$$->setClauseList = $5;
+				}
+			| SET insert_set_clause_group_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->setClauseList = $2;
+				}
+			| SET insert_set_clause_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->setClauseList = list_make1($2);
+				}
 		;
 
 override_kind:
@@ -13376,6 +13408,38 @@ set_target:
 set_target_list:
 			set_target								{ $$ = list_make1($1); }
 			| set_target_list ',' set_target		{ $$ = lappend($1,$3); }
+		;
+
+/*
+ * Grammar rules for INSERT ... SET syntax
+ * Supports both single-row and multi-row syntax:
+ *   Single row: INSERT INTO table SET col1=val1, col2=val2
+ *   Multi-row:  INSERT INTO table SET (col1=val1, col2=val2), (col1=val3, col2=val4)
+ *
+ * These rules are INSERT-specific and only allow simple column=value assignments.
+ */
+insert_set_clause:
+			set_target '=' a_expr
+				{
+					$1->val = (Node *) $3;
+					$$ = list_make1($1);
+				}
+		;
+
+insert_set_clause_list:
+			insert_set_clause						{ $$ = $1; }
+			| insert_set_clause_list ',' insert_set_clause
+													{ $$ = list_concat($1, $3); }
+		;
+
+insert_set_clause_group:
+			'(' insert_set_clause_list ')'			{ $$ = $2; }
+		;
+
+insert_set_clause_group_list:
+			insert_set_clause_group					{ $$ = list_make1($1); }
+			| insert_set_clause_group_list ',' insert_set_clause_group
+													{ $$ = lappend($1, $3); }
 		;
 
 

postgresql-cfbot/postgresql

src/backend/parser/gram.y
@@ -562,6 +562,8 @@ static Node *makeRecursiveViewSelect(char *relname, List *aliases, Node *query);
 %type <node>	for_portion_of_clause
 %type <node>	tablesample_clause opt_repeatable_clause
 %type <target>	target_el set_target insert_column_item
+%type <list>	insert_set_clause insert_set_clause_list
+%type <list>	insert_set_clause_group insert_set_clause_group_list
 
 %type <str>		generic_option_name
 %type <node>	generic_option_arg
@@ -13068,6 +13070,36 @@ insert_rest:
 					$$->cols = NIL;
 					$$->selectStmt = NULL;
 				}
+			| OVERRIDING override_kind VALUE_P SET insert_set_clause_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->override = $2;
+					$$->setClause = list_make1($5);
+				}
+			| OVERRIDING override_kind VALUE_P SET insert_set_clause_group_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->override = $2;
+					$$->setClause = $5;
+				}
+			| SET insert_set_clause_group_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->setClause = $2;
+				}
+			| SET insert_set_clause_list
+				{
+					$$ = makeNode(InsertStmt);
+					$$->cols = NIL;
+					$$->selectStmt = NULL;
+					$$->setClause = list_make1($2);
+				}
 		;
 
 override_kind:
@@ -13376,6 +13408,38 @@ set_target:
 set_target_list:
 			set_target								{ $$ = list_make1($1); }
 			| set_target_list ',' set_target		{ $$ = lappend($1,$3); }
+		;
+
+/*
+ * Grammar rules for INSERT ... SET syntax
+ * Supports both single-row and multi-row syntax:
+ *   Single row: INSERT INTO table SET col1=val1, col2=val2
+ *   Multi-row:  INSERT INTO table SET (col1=val1, col2=val2), (col1=val3, col2=val4)
+ *
+ * These rules are INSERT-specific and only allow simple column=value assignments.
+ */
+insert_set_clause:
+			set_target '=' a_expr
+				{
+					$1->val = (Node *) $3;
+					$$ = list_make1($1);
+				}
+		;
+
+insert_set_clause_list:
+			insert_set_clause						{ $$ = $1; }
+			| insert_set_clause_list ',' insert_set_clause
+													{ $$ = list_concat($1, $3); }
+		;
+
+insert_set_clause_group:
+			'(' insert_set_clause_list ')'			{ $$ = $2; }
+		;
+
+insert_set_clause_group_list:
+			insert_set_clause_group					{ $$ = list_make1($1); }
+			| insert_set_clause_group_list ',' insert_set_clause_group
+													{ $$ = lappend($1, $3); }
 		;
 
 
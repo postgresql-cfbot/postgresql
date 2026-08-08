hackorum-dev/postgres

libpq: Use pg_threads.h.
* adopt pg_threads.h for mutexes
* remove a local pthreads abstraction

Discussion:
Reviewed-by:

src/interfaces/libpq/fe-auth-oauth.c
@@ -28,6 +28,7 @@
 #include "mb/pg_wchar.h"
 #include "oauth-debug.h"
 #include "pg_config_paths.h"
+#include "port/pg_threads.h"
 #include "utils/memdebug.h"
 
 static PostgresPollingStatusType do_async(fe_oauth_state *state,
@@ -869,7 +870,7 @@ static int
 use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *request)
 {
 	static bool initialized = false;
-	static pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t init_mutex = PG_MTX_INIT;
 	int			lockerr;
 
 	void		(*init) (libpq_gettext_func gettext_impl);
@@ -946,7 +947,7 @@ use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *re
 		 * assigning them while another thread is executing the flows feels
 		 * like tempting fate.
 		 */
-		if ((lockerr = pthread_mutex_lock(&init_mutex)) != 0)
+		if ((lockerr = pg_mtx_lock(&init_mutex)) != pg_thrd_success)
 		{
 			/* Should not happen... but don't continue if it does. */
 			Assert(false);
@@ -972,7 +973,7 @@ use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *re
 			initialized = true;
 		}
 
-		pthread_mutex_unlock(&init_mutex);
+		pg_mtx_unlock(&init_mutex);
 	}
 
 	return (start_flow(conn, request) == 0) ? 1 : -1;

src/interfaces/libpq/fe-connect.c
@@ -35,6 +35,7 @@
 #include "mb/pg_wchar.h"
 #include "pg_config_paths.h"
 #include "port/pg_bswap.h"
+#include "port/pg_threads.h"
 
 #ifdef WIN32
 #include "win32.h"
@@ -54,12 +55,6 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <pwd.h>
-#endif
-
-#ifdef WIN32
-#include "pthread-win32.h"
-#else
-#include <pthread.h>
 #endif
 
 #ifdef USE_LDAP
@@ -8405,16 +8400,16 @@ pqParseProtocolVersion(const char *value, ProtocolVersion *result, PGconn *conn,
 static void
 default_threadlock(int acquire)
 {
-	static pthread_mutex_t singlethread_lock = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t singlethread_lock = PG_MTX_INIT;
 
 	if (acquire)
 	{
-		if (pthread_mutex_lock(&singlethread_lock))
+		if (pg_mtx_lock(&singlethread_lock) != pg_thrd_success)
 			Assert(false);
 	}
 	else
 	{
-		if (pthread_mutex_unlock(&singlethread_lock))
+		if (pg_mtx_unlock(&singlethread_lock) != pg_thrd_success)
 			Assert(false);
 	}
 }

src/interfaces/libpq/fe-misc.c
@@ -50,6 +50,7 @@
 #include "mb/pg_wchar.h"
 #include "pg_config_paths.h"
 #include "port/pg_bswap.h"
+#include "port/pg_threads.h"
 
 static int	pqPutMsgBytes(const void *buf, size_t len, PGconn *conn);
 static int	pqSendSome(PGconn *conn, int len);
@@ -1311,7 +1312,7 @@ libpq_binddomain(void)
 	 * might as well do it the same way everywhere.
 	 */
 	static volatile bool already_bound = false;
-	static pthread_mutex_t binddomain_mutex = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t binddomain_mutex = PG_MTX_INIT;
 
 	if (!already_bound)
 	{
@@ -1322,7 +1323,7 @@ libpq_binddomain(void)
 		int			save_errno = errno;
 #endif
 
-		(void) pthread_mutex_lock(&binddomain_mutex);
+		pg_mtx_lock(&binddomain_mutex);
 
 		if (!already_bound)
 		{
@@ -1339,7 +1340,7 @@ libpq_binddomain(void)
 			already_bound = true;
 		}
 
-		(void) pthread_mutex_unlock(&binddomain_mutex);
+		pg_mtx_unlock(&binddomain_mutex);
 
 #ifdef WIN32
 		SetLastError(save_errno);

src/interfaces/libpq/fe-secure-openssl.c
@@ -30,6 +30,7 @@
 #include "fe-auth.h"
 #include "fe-secure-common.h"
 #include "libpq-int.h"
+#include "port/pg_threads.h"
 
 #ifdef WIN32
 #include "win32.h"
@@ -43,12 +44,6 @@
 #endif
 
 #include <sys/stat.h>
-
-#ifdef WIN32
-#include "pthread-win32.h"
-#else
-#include <pthread.h>
-#endif
 
 /*
  * These SSL-related #includes must come after all system-provided headers.
@@ -83,7 +78,7 @@ static int	pgconn_bio_write(BIO *h, const char *buf, int size);
 static BIO_METHOD *pgconn_bio_method(void);
 static int	ssl_set_pgconn_bio(PGconn *conn);
 
-static pthread_mutex_t ssl_config_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pg_mtx_t ssl_config_mutex = PG_MTX_INIT;
 
 static PQsslKeyPassHook_OpenSSL_type PQsslKeyPassHook = NULL;
 static int	ssl_protocol_version_to_openssl(const char *protocol);
@@ -1835,7 +1830,7 @@ pgconn_bio_method(void)
 {
 	BIO_METHOD *res;
 
-	if (pthread_mutex_lock(&ssl_config_mutex))
+	if (pg_mtx_lock(&ssl_config_mutex) != pg_thrd_success)
 		return NULL;
 
 	res = pgconn_bio_method_ptr;
@@ -1865,13 +1860,13 @@ pgconn_bio_method(void)
 	}
 
 	pgconn_bio_method_ptr = res;
-	pthread_mutex_unlock(&ssl_config_mutex);
+	pg_mtx_unlock(&ssl_config_mutex);
 	return res;
 
 err:
 	if (res)
 		BIO_meth_free(res);
-	pthread_mutex_unlock(&ssl_config_mutex);
+	pg_mtx_unlock(&ssl_config_mutex);
 	return NULL;
 }
 

postgresql-cfbot/postgresql

src/interfaces/libpq/fe-auth-oauth.c
@@ -28,6 +28,7 @@
 #include "mb/pg_wchar.h"
 #include "oauth-debug.h"
 #include "pg_config_paths.h"
+#include "port/pg_threads_ext.h"
 #include "utils/memdebug.h"
 
 static PostgresPollingStatusType do_async(fe_oauth_state *state,
@@ -869,7 +870,7 @@ static int
 use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *request)
 {
 	static bool initialized = false;
-	static pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t init_mutex = PG_MTX_PLAIN_INIT;
 	int			lockerr;
 
 	void		(*init) (libpq_gettext_func gettext_impl);
@@ -946,7 +947,7 @@ use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *re
 		 * assigning them while another thread is executing the flows feels
 		 * like tempting fate.
 		 */
-		if ((lockerr = pthread_mutex_lock(&init_mutex)) != 0)
+		if ((lockerr = pg_mtx_lock(&init_mutex)) != pg_thrd_success)
 		{
 			/* Should not happen... but don't continue if it does. */
 			Assert(false);
@@ -972,7 +973,7 @@ use_builtin_flow(PGconn *conn, fe_oauth_state *state, PGoauthBearerRequestV2 *re
 			initialized = true;
 		}
 
-		pthread_mutex_unlock(&init_mutex);
+		pg_mtx_unlock(&init_mutex);
 	}
 
 	return (start_flow(conn, request) == 0) ? 1 : -1;

src/interfaces/libpq/fe-connect.c
@@ -35,6 +35,7 @@
 #include "mb/pg_wchar.h"
 #include "pg_config_paths.h"
 #include "port/pg_bswap.h"
+#include "port/pg_threads_ext.h"
 
 #ifdef WIN32
 #include "win32.h"
@@ -54,12 +55,6 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <pwd.h>
-#endif
-
-#ifdef WIN32
-#include "pthread-win32.h"
-#else
-#include <pthread.h>
 #endif
 
 #ifdef USE_LDAP
@@ -8408,16 +8403,16 @@ pqParseProtocolVersion(const char *value, ProtocolVersion *result, PGconn *conn,
 static void
 default_threadlock(int acquire)
 {
-	static pthread_mutex_t singlethread_lock = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t singlethread_lock = PG_MTX_PLAIN_INIT;
 
 	if (acquire)
 	{
-		if (pthread_mutex_lock(&singlethread_lock))
+		if (pg_mtx_lock(&singlethread_lock) != pg_thrd_success)
 			Assert(false);
 	}
 	else
 	{
-		if (pthread_mutex_unlock(&singlethread_lock))
+		if (pg_mtx_unlock(&singlethread_lock) != pg_thrd_success)
 			Assert(false);
 	}
 }

src/interfaces/libpq/fe-misc.c
@@ -50,6 +50,7 @@
 #include "mb/pg_wchar.h"
 #include "pg_config_paths.h"
 #include "port/pg_bswap.h"
+#include "port/pg_threads_ext.h"
 
 static int	pqPutMsgBytes(const void *buf, size_t len, PGconn *conn);
 static int	pqSendSome(PGconn *conn, int len);
@@ -1455,7 +1456,7 @@ libpq_binddomain(void)
 	 * might as well do it the same way everywhere.
 	 */
 	static volatile bool already_bound = false;
-	static pthread_mutex_t binddomain_mutex = PTHREAD_MUTEX_INITIALIZER;
+	static pg_mtx_t binddomain_mutex = PG_MTX_PLAIN_INIT;
 
 	if (!already_bound)
 	{
@@ -1466,7 +1467,7 @@ libpq_binddomain(void)
 		int			save_errno = errno;
 #endif
 
-		(void) pthread_mutex_lock(&binddomain_mutex);
+		pg_mtx_lock(&binddomain_mutex);
 
 		if (!already_bound)
 		{
@@ -1483,7 +1484,7 @@ libpq_binddomain(void)
 			already_bound = true;
 		}
 
-		(void) pthread_mutex_unlock(&binddomain_mutex);
+		pg_mtx_unlock(&binddomain_mutex);
 
 #ifdef WIN32
 		SetLastError(save_errno);

src/interfaces/libpq/fe-secure-openssl.c
@@ -31,6 +31,7 @@
 #include "fe-auth.h"
 #include "fe-secure-common.h"
 #include "libpq-int.h"
+#include "port/pg_threads_ext.h"
 
 #ifdef WIN32
 #include "win32.h"
@@ -44,12 +45,6 @@
 #endif
 
 #include <sys/stat.h>
-
-#ifdef WIN32
-#include "pthread-win32.h"
-#else
-#include <pthread.h>
-#endif
 
 /*
  * These SSL-related #includes must come after all system-provided headers.
@@ -84,7 +79,7 @@ static int	pgconn_bio_write(BIO *h, const char *buf, int size);
 static BIO_METHOD *pgconn_bio_method(void);
 static int	ssl_set_pgconn_bio(PGconn *conn);
 
-static pthread_mutex_t ssl_config_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pg_mtx_t ssl_config_mutex = PG_MTX_PLAIN_INIT;
 
 static PQsslKeyPassHook_OpenSSL_type PQsslKeyPassHook = NULL;
 static int	ssl_protocol_version_to_openssl(const char *protocol);
@@ -1866,7 +1861,7 @@ pgconn_bio_method(void)
 {
 	BIO_METHOD *res;
 
-	if (pthread_mutex_lock(&ssl_config_mutex))
+	if (pg_mtx_lock(&ssl_config_mutex) != pg_thrd_success)
 		return NULL;
 
 	res = pgconn_bio_method_ptr;
@@ -1896,13 +1891,13 @@ pgconn_bio_method(void)
 	}
 
 	pgconn_bio_method_ptr = res;
-	pthread_mutex_unlock(&ssl_config_mutex);
+	pg_mtx_unlock(&ssl_config_mutex);
 	return res;
 
 err:
 	if (res)
 		BIO_meth_free(res);
-	pthread_mutex_unlock(&ssl_config_mutex);
+	pg_mtx_unlock(&ssl_config_mutex);
 	return NULL;
 }
 